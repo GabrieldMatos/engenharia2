05ea94b699c8f1c8b6d07c1891186df04bcd0599
Gabriel de Matos Oliveira Fernandes <33075528+GabrieldMatos@users.noreply.github.com>
2018-05-01 18:28:36 -0300

    Atributo A3
    foi inerido o atributo A3 (String) e seu get e set.

--- teste.java
+++ teste.java
@@ -2,12 +2,18 @@ public class A {
   
     private int A1;
     private float A2;
+    private String A3;
 
     public int getA1(){
         return A1;
     }
+
     public float getA2(){
         return A2;
+    }
+
+     public String getA3(){
+        return A3;
     }
 
     public void setA1(int A1){
@@ -17,5 +23,8 @@ public class A {
     public void setA2(float A2){
         this.A2 = A2;
     }
-  
+
+    public void setA3(float A3){
+        this.A3 = A3;
+    }  
 }
