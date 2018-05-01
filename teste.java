934b8a636b40fad91f72c7fede2ddba670014640
Gabriel de Matos Oliveira Fernandes <33075528+GabrieldMatos@users.noreply.github.com>
2018-05-01 20:10:14 -0300

    atributo Aprinc4

--- teste.java
+++ teste.java
@@ -3,6 +3,7 @@ public class A {
     private int A1;
     private float A2;
     private String A3;
+    private String Aprinc4;
 
     public float soma(){
         return A1 + A2;
@@ -21,6 +22,10 @@ public class A {
         return A3;
     }
 
+    public String getAprinc4(){
+        return Aprinc4;
+    }
+
     public void setA1(int A1){
         this.A1 = A1;
     }
@@ -31,5 +36,9 @@ public class A {
 
     public void setA3(float A3){
         this.A3 = A3;
+    }  
+
+    public void setAprinc4(float Aprinc4){
+        this.Aprinc4 = Aprinc4;
     }  
 }
