0d9e213a9ce0e53ab3c214e423943cc960a9b497
Gabriel de Matos Oliveira Fernandes <33075528+GabrieldMatos@users.noreply.github.com>
2018-05-01 20:19:40 -0300

    class B

---
+++ B.java
@@ -0,0 +1,30 @@
+public class B {
+  
+    private int B1;
+    private float B2;
+    private String B4;
+
+    public int getB1(){
+        return B1;
+    }
+
+    public float getB2(){
+        return B2;
+    }
+    
+    public int getB4(){
+        return B4;
+    }
+
+    public void setB1(int B1){
+        this.B1 = B1;
+    }
+
+    public void setB2(float B2){
+        this.B2 = B2;
+    }
+    
+     public void setB4(String B4){
+        this.B4 = B4;
+    }
+}
